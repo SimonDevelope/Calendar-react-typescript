6b26c6ef1c09c9828d01d65294962c446b68e96e
SimonDevelope <whwnine@gmail.com>
2021-08-24 23:18:08 +0900

    #4. 달력 스타일링 완료 추후 달 이동 완료예정

--- src/Components/Month.tsx
+++ src/Components/Month.tsx
@@ -6,35 +6,40 @@ function month() {
 
   console.log(NAME_OF_DAYS);
   const day = new Date();
-  console.log(day);
-  const year = day.getFullYear();
-  console.log(year);
-  const month = day.getMonth(); // 원래 달보다 한 달 뒤로 나온다
-  console.log(month);
-  const isDay = day.getDate();
-  console.log(isDay);
-  const dayOfWeek = day.getDay(); // 0 ~ 6 (0: 일), (6: 토)
-  console.log(dayOfWeek);
-  const thisMonthFirstDay = new Date(year, month, 1); // 이번 달의 첫 날
-  console.log(thisMonthFirstDay);
-  const thisMonthLastDay = new Date(year, month + 1, 0); // 이번 달의 마지막 날
-  console.log(thisMonthLastDay);
-  const lastMonthFirstDay = new Date(year, month - 1, 1); // 지난 달의 첫 날
-  console.log(lastMonthFirstDay);
-  const lastMonthLastDay = new Date(year, month, 0); // 지난 달의 마지막 날
-  console.log(lastMonthLastDay);
-  const nextMonthFirstDay = new Date(year, month + 1, 1); // 다음 달의 첫 날
-  console.log(nextMonthFirstDay);
-  const nextMonthLastDay = new Date(year, month + 2, 0); // 다음 달의 마지막 날
-  console.log(nextMonthLastDay);
+  // console.log(day);
+  // const year = day.getFullYear();
+  // console.log(year);
+  // const month = day.getMonth(); // 원래 달보다 한 달 뒤로 나온다
+  // console.log(month);
+  // const isDay = day.getDate();
+  // console.log(isDay);
+  // const dayOfWeek = day.getDay(); // 0 ~ 6 (0: 일), (6: 토)
+  // console.log(dayOfWeek);
+
+  const thisMonthFirstDay = new Date(day.getFullYear(), day.getMonth(), 1); // 이번 달의 첫 날
+
+  const thisMonthLastDay = new Date(day.getFullYear(), day.getMonth() + 1, 0); // 이번 달의 마지막 날
+
+  const lastMonthFirstDay = new Date(day.getFullYear(), day.getMonth() - 1, 1); // 지난 달의 첫 날
+
+  const lastMonthLastDay = new Date(day.getFullYear(), day.getMonth(), 0); // 지난 달의 마지막 날
+
+  const nextMonthFirstDay = new Date(day.getFullYear(), day.getMonth(), 1); // 다음 달의 첫 날
+
+  const nextMonthLastDay = new Date(day.getFullYear(), day.getMonth() + 2, 0); // 다음 달의 마지막 날
+
+  console.log(lastMonthFirstDay, nextMonthFirstDay, nextMonthLastDay);
+
+  const thisYear = day.getFullYear();
+  const thisMonth = day.getMonth() + 1;
   // 여기까지 전체의 년월일 이 나오고 시간은 00시00분으로 맞춰진 상태가 나온다.
 
   // 이전날짜
-  const prevLastDate = new Date(year, month - 1, 0).getDate();
-  const prevLastDay = new Date(year, month - 1, 0).getDay();
+  const prevLastDate = new Date(day.getFullYear(), day.getMonth() - 1, 0).getDate();
+  const prevLastDay = new Date(day.getFullYear(), day.getMonth() - 1, 0).getDay();
 
-  const thisLastDate = new Date(year, month, 0).getDate();
-  const thisLastDay = new Date(year, month, 0).getDay();
+  const thisLastDate = new Date(day.getFullYear(), day.getMonth(), 0).getDate();
+  const thisLastDay = new Date(day.getFullYear(), day.getMonth(), 0).getDay();
 
   console.log(prevLastDate);
   console.log(prevLastDay);
@@ -56,27 +61,58 @@ function month() {
   for (let i = 1; i <= 13 - thisMonthLastDay.getDay(); i++) {
     dates.push(i);
   }
-  console.log(dates);
+
+  console.log(thisMonthFirstDay.setMonth(day.getMonth() - 1));
+
+  const prevNumberChecking = day.setMonth(day.getMonth() - 1);
+  console.log(prevNumberChecking);
+  const onPrevMonth: React.MouseEventHandler<HTMLButtonElement> = () => {
+    thisMonthLastDay.setMonth(day.getMonth() - 1);
+  };
+  const onNextMonth: React.MouseEventHandler<HTMLButtonElement> = () => {
+    day.setMonth(day.getMonth() + 1);
+    console.log(day.getMonth() + 1);
+  };
   return (
     <>
       <div className="total-view-port">
-        <div className="calendar-date-part">
-          {NAME_OF_DAYS.map((content: string, index) => {
-            return (
-              <div className="calendar-inner-attr" key={index}>
-                {content}
+        <div className="inner-total-view-port">
+          <div className="calendar-header-section">
+            <div>현재 시간</div>
+            <div className="calender-this-year-time">
+              <div className="calender-this-year-attr">{`${thisYear}년`}</div>
+              &nbsp;
+              <div className="calender-this-month-attr">{`${thisMonth}월`}</div>
+            </div>
+            <div className="calendar-month-control-wrapper">
+              <button className="calendar-month-control-button-attr" onClick={onPrevMonth}>{`<`}</button>
+              <div className="calender-month-area">
+                <div className="calendar-month-control-time-attr">8월</div>
               </div>
-            );
-          })}
-        </div>
-        <div className="calendar-number-part">
-          {dates.map((datesList, index) => {
-            return (
-              <div key={index} className="calender-number-inner-attr">
-                {datesList}
-              </div>
-            );
-          })}
+              <button className="calendar-month-control-button-attr" onClick={onNextMonth}>{`>`}</button>
+            </div>
+          </div>
+          <div className="partition-with-calender-header"></div>
+          <div className="calendar-contents-wrapper">
+            <div className="calendar-date-part">
+              {NAME_OF_DAYS.map((content: string, index) => {
+                return (
+                  <div className="calendar-inner-attr" key={index}>
+                    {content}
+                  </div>
+                );
+              })}
+            </div>
+            <div className="calendar-number-part">
+              {dates.map((datesList, index) => {
+                return (
+                  <div key={index} className="calender-number-inner-attr">
+                    {datesList}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </>
